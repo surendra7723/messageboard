0540b29b5ac86ed4a10a60a88c53066186522e99
SurendraCodes <surendra7723@mbmcsit.edu.np>
2024-02-17 20:09:38 +0545

    commit message

---
+++ posts/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from .models import Post
+from django.urls import reverse 
+from .models import Post
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        
+    
